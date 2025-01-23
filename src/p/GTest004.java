2160fb249fe8302aa7d4d45ceda82b76c103d381
AnuragTesterOne <68826268+AnuragTesterOne@users.noreply.github.com>
2025-01-23 14:23:52 +0530

    Update GTest004.java

--- src/p/GTest004.java
+++ src/p/GTest004.java
@@ -5,9 +5,7 @@ public class GTest001
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcome to github1111111111111");
-		System.out.println("Welcome to github1111111111111");
-		System.out.println("Welcome to github1111111111111");
+		System.out.println("I errased you code ha ha ha");
 	}
 
 }
